39d552cbbedcc191ecf8920180950e291fd98bfc
PabloFC <pablofc_68@hotmail.com>
2024-06-18 12:00:01 +0200

    Navbar

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,29 @@
+import { Box } from "@mui/material";
+import Navbar from "../components/Navbar";
+
+const drawerWidth = 240;
+
+const JournalLayout = ({ children }) => {
+  return (
+    <Box sx={{ display: "flex" }}>
+      {/* Navbar drawerWidth */}
+      <Navbar drawerWidth={drawerWidth} />
+
+      {/* Sidebar drawerWidth */}
+
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+        }}
+      >
+        {/* Toolbar */}
+
+        {children}
+      </Box>
+    </Box>
+  );
+};
+
+export default JournalLayout;
--- src/journal/pages/JournalPages.jsx
+++ src/journal/pages/JournalPages.jsx
@@ -1,10 +1,17 @@
 import { Typography } from "@mui/material";
+import JournalLayout from "../layout/JournalLayout";
 
 const JournalPages = () => {
   return (
-    <div>
-      <Typography variant="h1">JournalPage</Typography>
-    </div>
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci,
+        error incidunt odit ex labore cum, id expedita dignissimos consequatur
+        fuga atque ea dolore suscipit praesentium nobis omnis enim a. Dolor?
+      </Typography>
+      {/* NothingSelected */}
+      {/* NoteView */}
+    </JournalLayout>
   );
 };
 
