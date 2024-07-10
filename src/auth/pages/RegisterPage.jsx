fd481dad7e3c4d3f604e7df27072219150565d21
PabloFC <pablofc_68@hotmail.com>
2024-07-10 14:12:29 +0200

    register form

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,11 +1,24 @@
 import { Grid, TextField, Typography, Button, Link } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 import AuthLayout from "../layout/AuthLayout";
+import { useForm } from "../../hooks/useForm";
 
+const formData = {
+  email: "pfyc88@google.com",
+  password: "1234",
+  displayName: "PabloFC",
+};
 const RegisterPage = () => {
+  const { displayName, email, password, onInputChange, formState } =
+    useForm(formData);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(formState);
+  };
   return (
     <AuthLayout title="Crear cuenta">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
@@ -13,6 +26,9 @@ const RegisterPage = () => {
               type="text"
               placeholder="Nombre"
               fullWidth
+              name="displayName"
+              value={displayName}
+              onChange={onInputChange}
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
@@ -21,6 +37,9 @@ const RegisterPage = () => {
               type="email"
               placeholder="Correo@google.com"
               fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
             />
           </Grid>
 
@@ -30,12 +49,15 @@ const RegisterPage = () => {
               type="password"
               placeholder="contraseña"
               fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
             />
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12}>
-              <Button variant="contained" fullWidth>
+              <Button type="submit" variant="contained" fullWidth>
                 Crear cuenta
               </Button>
             </Grid>
